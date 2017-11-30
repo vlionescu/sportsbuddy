4443ac362b68bf9966336573e05846e9059d38e6
Vlad M Ionescu <VladM.Ionescu@endava.com>
2017-11-30 11:34:54 +0200

    login design

--- src/app/modules/authentication/services/auth.service.ts
+++ src/app/modules/authentication/services/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+// const bgImages = {
+//   login: 'football.jpg',
+//   register: 'basketball.jpg',
+//   validate: 'tennis.jpg',
+//   forgotPassword: 'volleyball.jpg',
+//   changePassword: 'nfl.jpg'
+// };
+
 @Injectable()
 export class AuthService {
 
   constructor() { }
-
+  // static getBgImage(route) {
+  //   return bgImages[route]
+  // }
 }
